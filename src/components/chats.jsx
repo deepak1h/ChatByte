71d7a87ad54ee6ee1ba86c4ff1139964ec98be7b
deepak1h <deepakkumarpandeychs@gmail.com>
2023-09-23 23:58:41 +0530

    back U7: user can make friend and to chat

--- src/components/chats.jsx
+++ src/components/chats.jsx
@@ -1,147 +1,47 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Profile from '../image/profile.png'
 import '../css/chats.css'
+import {AuthContext} from "../context/authContext"
+import { doc, onSnapshot } from 'firebase/firestore'
+import { db } from '../firebase'
 
 const Chats = () => {
+
+  const [chats, setChats] = useState([])
+  const {currentUser} = useContext(AuthContext)
+
+  useEffect(()=> {
+    
+    const getChats = () => {
+
+      const unsubscribe = onSnapshot(doc(db,"userChats", currentUser.uid), (doc)=>{
+      setChats(doc.data())
+    })
+
+    return () => {
+      unsubscribe();
+    };
+  };
+
+    currentUser.uid && getChats()
+
+  },[currentUser.uid]) 
+
+  console.log(chats)
+
   return (
-    <div className='chats'>
-      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
 
-          <span >Deepak</span>
-          <p>latest message</p>
-
+    <div className='chats'>  
+      {Object.entries(chats)?.map(([chatID, chatData]) =>(
+        <div className='userChat' key={chatID}>
+          <img src={chatData.userInfo.photoURL}></img>
+          <div className='userChatInfo'>
+            <span >{chatData.userInfo.name}</span>
+            <p>{chatData.userInfo.latestmessage}</p>
+          </div>
         </div>
-      </div>
-      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>
-      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>
-      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>
-      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>      <div className='userChat'>
-        <img src={Profile}></img>
-        <div className='userChatInfo'>
-
-          <span >Deepak</span>
-          <p>latest message</p>
-
-        </div>
-      </div>
+      ))}  
     </div>
-
-
-
   )
 }
 
